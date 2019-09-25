b10a0609980ed811a08f870cae983f73b35709db
smellyung <kelly.ung@centrica.com>
2019-09-25 21:13:43 +0100

    Updates

---
+++ precache-manifest.c1de68fa817c5ab157efd6edea24df23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786e226c12a519e204e365c20531e080",
+    "url": "/kellys-portfolio/index.html"
+  },
+  {
+    "revision": "ef973d444fcfa00b3fff",
+    "url": "/kellys-portfolio/static/css/main.2a46a89e.chunk.css"
+  },
+  {
+    "revision": "bea712fa06641ea52238",
+    "url": "/kellys-portfolio/static/js/2.5de5c3bb.chunk.js"
+  },
+  {
+    "revision": "ef973d444fcfa00b3fff",
+    "url": "/kellys-portfolio/static/js/main.c1f44d6c.chunk.js"
+  },
+  {
+    "revision": "d3c710028f394eb15499",
+    "url": "/kellys-portfolio/static/js/runtime~main.eb1772f5.js"
+  },
+  {
+    "revision": "a172e4204573f5058b1fb5ac406f7271",
+    "url": "/kellys-portfolio/static/media/profile.a172e420.jpg"
+  },
+  {
+    "revision": "55e13fd6c19c2c81db2778f270ef4681",
+    "url": "/kellys-portfolio/static/media/sunset.55e13fd6.jpg"
+  }
+]);
